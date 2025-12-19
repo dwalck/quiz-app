0f28ec9275bae117cc4986c283449992f16a7a78
dwalck <bodacz6512@gmail.com>
2025-12-19 22:30:05 +0100

    CannotChangeQuizStateException test

---
+++ backend/tests/Quiz/Domain/Exception/CannotChangeQuizStateExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Quiz\Domain\Exception;
+
+use App\Quiz\Domain\Enum\QuizState;
+use App\Quiz\Domain\Exception\CannotChangeQuizStateException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(CannotChangeQuizStateException::class)]
+final class CannotChangeQuizStateExceptionTest extends TestCase
+{
+    public function test_exception_message_is_valid(): void
+    {
+        $this->expectExceptionMessage('Cannot change quiz state from "STARTED" to "FINISHED".');
+
+        throw new CannotChangeQuizStateException(
+            QuizState::STARTED,
+            QuizState::FINISHED
+        );
+    }
+}

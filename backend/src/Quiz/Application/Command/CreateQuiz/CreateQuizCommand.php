9bd4eb3c04498777b6d2e3b0fa3988a2cb3cec64
dwalck <bodacz6512@gmail.com>
2025-12-28 22:20:36 +0100

    use QuizId on command

--- backend/src/Quiz/Application/Command/CreateQuiz/CreateQuizCommand.php
+++ backend/src/Quiz/Application/Command/CreateQuiz/CreateQuizCommand.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Quiz\Application\Command\CreateQuiz;
 
+use App\Quiz\Domain\ValueObject\Quiz\QuizId;
 use Symfony\Component\Messenger\Attribute\AsMessage;
-use Symfony\Component\Uid\Uuid;
 
 #[AsMessage]
 final class CreateQuizCommand
 {
     public function __construct(
-        public Uuid $quizId,
+        public QuizId $quizId,
         public int $questionsCount,
         public int $duration = 60,
         public int $passingScore = 70,

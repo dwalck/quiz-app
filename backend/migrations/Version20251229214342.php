0391f03940d8fc195b73c14a28ce561a1c4dc474
dwalck <bodacz6512@gmail.com>
2025-12-29 22:44:02 +0100

    Update doctrine/dbal to 4.0.x

---
+++ backend/migrations/Version20251229214342.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251229214342 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE question (id UUID NOT NULL, content VARCHAR(2000) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE TABLE question_answer (id UUID NOT NULL, content VARCHAR(2000) NOT NULL, correct BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, question_id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_DD80652D1E27F6BF ON question_answer (question_id)');
+        $this->addSql('CREATE TABLE quiz (id UUID NOT NULL, configuration_duration INT NOT NULL, configuration_passing_score INT NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE TABLE quiz_question (answered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, id UUID NOT NULL, quiz_id UUID NOT NULL, question_id UUID NOT NULL, answer_id UUID DEFAULT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_6033B00B853CD175 ON quiz_question (quiz_id)');
+        $this->addSql('CREATE INDEX IDX_6033B00B1E27F6BF ON quiz_question (question_id)');
+        $this->addSql('CREATE INDEX IDX_6033B00BAA334807 ON quiz_question (answer_id)');
+        $this->addSql('CREATE TABLE quiz_state_change (id UUID NOT NULL, state VARCHAR(32) NOT NULL, changed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, quiz_id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_9F6AA474853CD175 ON quiz_state_change (quiz_id)');
+        $this->addSql('ALTER TABLE question_answer ADD CONSTRAINT FK_DD80652D1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) NOT DEFERRABLE');
+        $this->addSql('ALTER TABLE quiz_question ADD CONSTRAINT FK_6033B00B853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id) NOT DEFERRABLE');
+        $this->addSql('ALTER TABLE quiz_question ADD CONSTRAINT FK_6033B00B1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) NOT DEFERRABLE');
+        $this->addSql('ALTER TABLE quiz_question ADD CONSTRAINT FK_6033B00BAA334807 FOREIGN KEY (answer_id) REFERENCES question_answer (id) NOT DEFERRABLE');
+        $this->addSql('ALTER TABLE quiz_state_change ADD CONSTRAINT FK_9F6AA474853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id) NOT DEFERRABLE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE question_answer DROP CONSTRAINT FK_DD80652D1E27F6BF');
+        $this->addSql('ALTER TABLE quiz_question DROP CONSTRAINT FK_6033B00B853CD175');
+        $this->addSql('ALTER TABLE quiz_question DROP CONSTRAINT FK_6033B00B1E27F6BF');
+        $this->addSql('ALTER TABLE quiz_question DROP CONSTRAINT FK_6033B00BAA334807');
+        $this->addSql('ALTER TABLE quiz_state_change DROP CONSTRAINT FK_9F6AA474853CD175');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE question_answer');
+        $this->addSql('DROP TABLE quiz');
+        $this->addSql('DROP TABLE quiz_question');
+        $this->addSql('DROP TABLE quiz_state_change');
+    }
+}

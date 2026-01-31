eec6935dd283d28533bc5519339eab07146d8d8f
dwalck <bodacz6512@gmail.com>
2026-01-31 21:51:13 +0100

    CommandDispatcher - throw wrapped exception instead of messenger exception

--- backend/src/SharedKernel/Infrastructure/Symfony/CommandDispatcher.php
+++ backend/src/SharedKernel/Infrastructure/Symfony/CommandDispatcher.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\SharedKernel\Infrastructure\Symfony;
 
 use App\SharedKernel\Application\CommandDispatcherInterface;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Webmozart\Assert\Assert;
 
 final readonly class CommandDispatcher implements CommandDispatcherInterface
 {
@@ -16,6 +18,14 @@ final readonly class CommandDispatcher implements CommandDispatcherInterface
 
     public function dispatch(object $command): void
     {
-        $this->messageBus->dispatch($command);
+        try {
+            $this->messageBus->dispatch($command);
+        } catch (HandlerFailedException $e) {
+            $exceptions = $e->getWrappedExceptions();
+
+            Assert::notEmpty($exceptions);
+
+            throw \reset($exceptions);
+        }
     }
 }
